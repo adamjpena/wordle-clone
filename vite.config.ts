5752b0509d7a09faaca6afc217d35979ce5884e0
Adam Peña <dev-no-reply@adamjpena.com>
2026-07-15 04:57:19 -0500

    chore: migrate app from CRA to Vite

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  base: '/wordle-clone/',
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/setupTests.ts',
+  },
+});
